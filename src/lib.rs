5f6c5f537514e06b19ed7b17426296b4374d22bb
Koen Bolhuis <koen.bolhuis@gmail.com>
2021-01-20 00:37:39 +0100

    Improve crate documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,14 @@
 //! API bindings for ListenBrainz.
+//!
+//! This crate aims to be an idiomatic wrapper of the [ListenBrainz HTTP API] (version 1).
+//! It contains functionality for direct access to the API in the [`raw`] module, as well
+//! as a more convenient [`ListenBrainz`] client which is easier to use.
+//!
+//! Generally, using the `raw` functionality is more cumbersome, as its types and functions
+//! map one-to-one to the HTTP API's JSON input- and response data. Using the `ListenBrainz`
+//! type is therefore recommended.
+//!
+//! [ListenBrainz HTTP API]: https://listenbrainz.readthedocs.io/en/production/dev/api/
 
 mod error;
 pub mod raw;
