08cc84da37cf6a239a22dd131609195863755d1c
Koen Bolhuis <koen.bolhuis@gmail.com>
2023-02-07 08:35:45 +0100

    Fix clippy lints

--- src/raw/endpoint.rs
+++ src/raw/endpoint.rs
@@ -24,23 +24,23 @@ impl<'a> fmt::Display for Endpoint<'a> {
             Self::SubmitListens => "submit-listens",
             Self::ValidateToken => "validate-token",
             Self::DeleteListen => "delete-listen",
-            Self::UserListenCount(user) => return write!(f, "user/{}/listen-count", user),
-            Self::UserPlayingNow(user) => return write!(f, "user/{}/playing-now", user),
-            Self::UserListens(user) => return write!(f, "user/{}/listens", user),
+            Self::UserListenCount(user) => return write!(f, "user/{user}/listen-count"),
+            Self::UserPlayingNow(user) => return write!(f, "user/{user}/playing-now"),
+            Self::UserListens(user) => return write!(f, "user/{user}/listens"),
             Self::LatestImport => "latest-import",
             Self::StatsSitewideArtists => "stats/sitewide/artists",
             Self::StatsUserListeningActivity(user) => {
-                return write!(f, "stats/user/{}/listening-activity", user)
+                return write!(f, "stats/user/{user}/listening-activity")
             }
             Self::StatsUserDailyActivity(user) => {
-                return write!(f, "stats/user/{}/daily-activity", user)
+                return write!(f, "stats/user/{user}/daily-activity")
             }
-            Self::StatsUserRecordings(user) => return write!(f, "stats/user/{}/recordings", user),
-            Self::StatsUserArtistMap(user) => return write!(f, "stats/user/{}/artist-map", user),
-            Self::StatsUserReleases(user) => return write!(f, "stats/user/{}/releases", user),
-            Self::StatsUserArtists(user) => return write!(f, "stats/user/{}/artists", user),
+            Self::StatsUserRecordings(user) => return write!(f, "stats/user/{user}/recordings"),
+            Self::StatsUserArtistMap(user) => return write!(f, "stats/user/{user}/artist-map"),
+            Self::StatsUserReleases(user) => return write!(f, "stats/user/{user}/releases"),
+            Self::StatsUserArtists(user) => return write!(f, "stats/user/{user}/artists"),
             Self::StatusGetDumpInfo => "status/get-dump-info",
         };
-        write!(f, "{}", s)
+        write!(f, "{s}")
     }
 }
