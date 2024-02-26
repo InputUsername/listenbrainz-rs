04237234dcb0204fc9941ead83da9891700e32e0
Marc Plano-Lesay <kernald@enoent.fr>
2024-02-26 21:39:36 +1100

    Add algorithm metadata to playlists

--- src/raw/jspf.rs
+++ src/raw/jspf.rs
@@ -41,6 +41,19 @@ pub struct MusicBrainzPlaylistExtension {
     pub copied_from_deleted: bool,
     pub public: bool,
     pub last_modified_at: String,
+    pub additional_metadata: AdditionalMetadata,
+}
+
+/// Type of the [`MusicBrainzPlaylistExtension::additional_metadata`] field.
+#[derive(Debug, Deserialize, Serialize)]
+pub struct AdditionalMetadata {
+    pub algorithm_metadata: Option<AlgorithmMetadata>,
+}
+
+/// Type of the [`AdditionalMetadata::algorithm_metadata`] field.
+#[derive(Debug, Deserialize, Serialize)]
+pub struct AlgorithmMetadata {
+    pub source_patch: Option<String>,
 }
 
 /// Type of the [`PlaylistInfo::track`] field.
