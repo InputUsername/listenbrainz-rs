b02278e17a8525d11a84a4fcfaacd997241cfcc5
RustyNova <rusty.nova.jsb@gmail.com>
2025-02-12 08:13:12 +0000

    feat: added art/grid API endpoint

--- src/raw/request.rs
+++ src/raw/request.rs
@@ -148,3 +148,87 @@ impl Borrow<str> for Empty {
         unreachable!("Should never be used as a value")
     }
 }
+
+// --- Art Requests ---
+#[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq)]
+pub struct ArtGrid {
+    /// The background for the cover art
+    pub background: ArtGridBackground,
+
+    /// The size of the cover art image
+    pub image_size: u64,
+
+    /// The dimension to use for this grid. A grid of dimension 3 has 3 images across and 3 images down, for a total of 9 images.
+    pub dimension: u64,
+
+    /// If cover art is missing for a given release_mbid, skip it and move on to the next one, if true is passed.
+    /// If false, the show-caa option will decide what happens.
+    #[serde(rename = "skip-missing")]
+    pub skip_missing: bool,
+
+    /// If cover art is missing and skip-missing is false,
+    /// then show-caa will determine if a blank square is shown or if the Cover Art Archive missing image is shown.
+    #[serde(rename = "show-caa")]
+    pub show_caa: bool,
+
+    /// The tiles parameter is a list of strings that determines the location where cover art images should be placed.
+    /// Each string is a comma separated list of image cells.
+    /// A grid of dimension 3 has 9 cells, from 0 in the upper left hand corner, 2 in the upper right hand corner,
+    /// 6 in the lower left corner and 8 in the lower right corner.
+    ///
+    /// ```plaintext
+    /// 0 1 2
+    /// 3 4 5
+    /// 6 7 8
+    /// ```
+    ///
+    /// Specifying only a single cell will have the image cover that cell exactly.
+    /// If more than one cell is specified, the image will cover the area defined by the bounding box of all the given cells.
+    /// These tiles only define bounding box areas – no clipping of images that may fall outside of these tiles will be performed.
+    ///
+    /// ## Exemple
+    /// For this exemple, the dimension is set to 3
+    /// ```
+    /// let tiles = vec![
+    ///     "0, 1, 3, 4".to_string(), // The first image cover the slots 0, 1, 3, and 4
+    ///     "2".to_string(),
+    ///     "5".to_string(),
+    ///     "6".to_string(),
+    ///     "7".to_string(),
+    ///     "8".to_string(),
+    /// ];
+    /// ```
+    ///
+    /// This will result in this arrangement:
+    ///
+    /// ```plaintext
+    /// 1 1 2
+    /// 1 1 3
+    /// 4 5 6
+    /// ```
+    pub tiles: Option<Vec<String>>,
+
+    /// An ordered list of release_mbids. The images will be loaded and processed in the order that this list is in.
+    /// The cover art for the release_mbids will be placed on the tiles defined by the tiles parameter.
+    /// If release_group_mbids are supplied as well, ONLY cover arts for release_group_mbids will be processed.
+    pub release_mbids: Option<Vec<String>>,
+
+    /// An ordered list of release_group_mbids. The images will be loaded and processed in the order that this list is in.
+    /// The cover art for the release_group_mbids will be placed on the tiles defined by the tiles parameter.
+    /// If release_mbids are supplied as well, ONLY cover arts for release_mbids will be processed.
+    pub release_group_mbids: Option<Vec<String>>,
+
+    /// Size in pixels of each cover art in the composited image. Can be either 250 or 500
+    pub cover_art_size: Option<u64>,
+}
+
+/// The background for the cover art
+#[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq)]
+pub enum ArtGridBackground {
+    #[serde(rename = "transparent")]
+    Transparent,
+    #[serde(rename = "white")]
+    White,
+    #[serde(rename = "black")]
+    Black,
+}
